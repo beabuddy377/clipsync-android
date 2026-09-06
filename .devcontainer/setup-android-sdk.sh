fc9155c148a6dc58168eeac6371d5a029a57face
beabuddy377 <beanbuddy3@gmail.com>
2026-09-06 09:50:23 +0530

    Update and rename setup-android-sdk.sh: to setup-android-sdk.sh

--- .devcontainer/setup-android-sdk.sh
+++ .devcontainer/setup-android-sdk.sh
@@ -6,9 +6,11 @@ unzip -q cmdline-tools.zip
 mv cmdline-tools latest
 rm cmdline-tools.zip
 
+# Script ke andar PATH set karna zaroori hai
+export PATH=$PATH:$ANDROID_HOME/cmdline-tools/latest/bin:$ANDROID_HOME/platform-tools
+
 echo 'export ANDROID_HOME=$HOME/android-sdk' >> ~/.bashrc
 echo 'export PATH=$PATH:$ANDROID_HOME/cmdline-tools/latest/bin:$ANDROID_HOME/platform-tools' >> ~/.bashrc
-source ~/.bashrc
 
 yes | sdkmanager --licenses
 sdkmanager "platform-tools" "platforms;android-34" "build-tools;34.0.0"
